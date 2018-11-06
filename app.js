813db92237cd07da589b2c71cde1bc408e6d691e
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2018-11-07 01:06:28 +0545

    Encoding User Input and Yargs

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+var argv = yargs
+    .options({
+        a: {
+            describe: "Address to fetch Weather for",
+            demand: true,
+            alias: 'address',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv.address);
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({ 
-    url: 'https://nominatim.openstreetmap.org/search.php?q=1301+lombard+street+philadelphia&format=json',
+    url: `https://nominatim.openstreetmap.org/search.php?q=${encodedAddress}&format=json`,
     json: true,
     headers: {
         'User-Agent': 'request'
